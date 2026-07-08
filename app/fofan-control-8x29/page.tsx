882492cb8f85bd94469291f855a5c03265f9ede1
4fan777 <4fan777@users.noreply.github.com>
2026-07-08 20:11:11 +0300

    Disable caching for admin page

--- app/fofan-control-8x29/page.tsx
+++ app/fofan-control-8x29/page.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   },
 };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function HiddenAdminPage({
   searchParams,
 }: {
